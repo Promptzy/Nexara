e0e9b937cf7d74dbcf60b61910576eab5bf25b42
Rushikesh Phadtare <rushikeshphadtare2003@gmail.com>
2025-08-03 12:51:48 +0530

    cleanup: remove test files

--- frontend/test-formatting.js
+++
@@ -1,4 +0,0 @@
-// Test file with bad formatting
-const badlyFormatted = 'no spaces'
-const tooManySpaces = 'too many spaces'
-console.log(badlyFormatted, tooManySpaces)
--- frontend/test-formatting2.js
+++
@@ -1,1 +0,0 @@
-const test="bad formatting";console.log(test)
