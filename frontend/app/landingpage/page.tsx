0cef59be767f20431381ce4915b27f6c4943e8eb
Pranjal6955 <negipranjal555@gmail.com>
2025-08-05 02:10:26 +0530

    Implemented Navbar

--- frontend/app/landingpage/page.tsx
+++ frontend/app/landingpage/page.tsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import Navbar from './section/Navbar';
+import Home from './section/Home'
+import Threads from './components/Threads';
 
 const page = () => {
   return (
-    <main className="bg-black min-h-screen">
-      <Navbar />
+    <main className="bg-black min-h-screen relative">
+      {/* Threads background */}
+      <div className="fixed inset-0 w-full h-full z-0">
+        <Threads
+          amplitude={1}
+          distance={0}
+          enableMouseInteraction={true}
+        />
+      </div>
+      
+      {/* Content layer */}
+      <div className="relative z-10">
+        <Navbar />
+        <Home />
+      </div>
     </main>
   )
 }
